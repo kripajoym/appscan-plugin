43115301ed99fe382685778446c340c11a259dd2
kripajoym <36063366+kripajoym@users.noreply.github.com>
2026-07-13 15:24:38 +0530

    Update FailureCondition.java

--- src/main/java/com/hcl/appscan/jenkins/plugin/results/FailureCondition.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/results/FailureCondition.java
@@ -21,7 +21,7 @@ import org.kohsuke.stapler.QueryParameter;
 import com.hcl.appscan.jenkins.plugin.Messages;
 
 public class FailureCondition extends AbstractDescribableImpl<FailureCondition> implements Serializable {
-	//changes to teest joy
+	//changes to teest
 	
 	private static final long serialVersionUID = 1L;
 	
