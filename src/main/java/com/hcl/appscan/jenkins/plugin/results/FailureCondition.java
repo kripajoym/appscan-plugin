67283c68b0e09ad06945fdfd02183c08dea01fcc
kripajoym <36063366+kripajoym@users.noreply.github.com>
2026-07-13 13:24:16 +0530

    Update FailureCondition.java

--- src/main/java/com/hcl/appscan/jenkins/plugin/results/FailureCondition.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/results/FailureCondition.java
@@ -21,7 +21,7 @@ import org.kohsuke.stapler.QueryParameter;
 import com.hcl.appscan.jenkins.plugin.Messages;
 
 public class FailureCondition extends AbstractDescribableImpl<FailureCondition> implements Serializable {
-	//changes to test action
+	//changes to test a
 	
 	private static final long serialVersionUID = 1L;
 	
