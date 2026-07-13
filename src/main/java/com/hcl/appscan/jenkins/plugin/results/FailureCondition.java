d882cde67ad455c8468aa598df3a29d51f136f9a
kripajoym <36063366+kripajoym@users.noreply.github.com>
2026-07-13 15:00:19 +0530

    trial

--- src/main/java/com/hcl/appscan/jenkins/plugin/results/FailureCondition.java
+++ src/main/java/com/hcl/appscan/jenkins/plugin/results/FailureCondition.java
@@ -21,7 +21,7 @@ import org.kohsuke.stapler.QueryParameter;
 import com.hcl.appscan.jenkins.plugin.Messages;
 
 public class FailureCondition extends AbstractDescribableImpl<FailureCondition> implements Serializable {
-	//changes to test a
+	//changes to test
 	
 	private static final long serialVersionUID = 1L;
 	
